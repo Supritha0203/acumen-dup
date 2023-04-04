1d71f52255de64a9683905cf552fc0732e19197c
Suhaib Ahmed <suhaibahmed033@gmail.com>
2023-04-05 01:30:44 +0530

    Posters

--- constants/index.js
+++ constants/index.js
@@ -35,40 +35,45 @@ export const exploreWorlds4 = [
   {
     id: 'world-1',
     imgUrl: '/planet-09.png',
-    Poster: '/planet-09.png',
+    Poster: '/poster_soccer.png',
     title: 'Robo Soccer',
+    subtitle: 'Robot Soccer is a robot sport, where robot machines compete against each other in a variation of the game of Association Football (soccer). In robot soccer, each team is fully autonomous.',
     open:false
     // pageUrl: '/Poster'
   },
   {
     id: 'world-2',
     imgUrl: '/RE-1.png',
-    Poster: '/RE-1.png',
+    Poster: '/poster_robocontest.png',
     title: 'Autonomous Robo Contest',
+    subtitle: 'The main theme of the RACE is to build a completely autonomous robot which is capable of solving an arena which consists of various aspects like line following, obstacle detection.',
     open:false
     // pageUrl: '/InspireAndIgnite'
   },
   {
     id: 'world-3',
     imgUrl: '/RE-3.png',
-    Poster: '/RE-3.png',
+    Poster: '/poster_basketball.png',
     title: 'Robo Basketball',
+    subtitle: 'This is a technical team event. This event will be played by robots of two teams against each other. The main objective of the bots will be to score goals by throwing the ball in the provided basket with the help of a robotic arm.',
     open:false
     // pageUrl: '/ProjectExpo'
   },
   {
     id: 'world-4',
     imgUrl: '/RE-5.jpg',
-    Poster: '/RE-5.jpg',
+    Poster: '/poster_drones.png',
     title: 'Drones',
+    subtitle: 'Drones were something straight out of science fiction which became reality now. All of us will want to experience flying it.  Well this is an opportunity to get a real time experience of flying and controlling a drone.it is like a workshop conducted by our students. You will first get virtual controlling experience with simulator upon training you will be given chance to fly real drone. So you will learn how to design and control the drone.',
     open:false
     // pageUrl: '/RoboticEvents'
   },
   {
     id: 'world-5',
     imgUrl: '/RE-6.jpg',
-    Poster: '/RE-6.jpg',
+    Poster: '/poster_rcplanes.png',
     title: 'RC Planes',
+    subtitle: 'RC Planes race is conducted to compete for the blast the target balloon allocated. This event expects a designed RC plane based on your own specifications. ',
     open:false
     // pageUrl: '/TechnicalEvents'  
   },
@@ -80,38 +85,43 @@ export const exploreWorlds5 = [
     imgUrl: '/planet13.jpg',
     Poster: '/planet13.jpg',
     title: 'Industry Defined Problem',
+    subtitle: 'In this event you need to provide ideas for the given industry problems to jury persons.The below link contains the list of problem statements provided by different organizations.',
     open:false
     // pageUrl: '/Poster'
   },
   {
     id: 'world-2',
     imgUrl: '/planet-02.png',
-    Poster: '/planet-02.png',
+    Poster: '/poster_foxhunt.png',
     title: 'Fox Hunt',
+    subtitle: 'Foxhunt is the simultaneous operation of two devices with overlapping frequencies. We have certain boundary ranges and few set of rules for this event. In this, we conceal the transmitter, and each team is given a receiver. They should adjust the frequency of the receiver and move in accordance with it. When the transmitter and receiver frequencies coincide, a buzzer sound is heard through headphones and as you get closer to the transmitter the sound will get clearer and louder. So your task is with the provided receiver, the people must look for the hidden transmitter.',
     open:false
     // pageUrl: '/InspireAndIgnite'
   },
   {
     id: 'world-3',
     imgUrl: '/Planet24.jpg',
-    Poster: '/Planet24.jpg',
+    Poster: '/poster_debugging.png',
     title: 'Circuit Debugging',
+    subtitle: 'The task is to build a circuit in tinker cad which is an online simulation tool which helps in programming the Arduino,check if the circuit which we are building is correct and give the output required from the circuit.',
     open:false
     // pageUrl: '/ProjectExpo'
   },
   {
     id: 'world-4',
     imgUrl: '/Planet25.jpg',
-    Poster: '/Planet25.jpg',
+    Poster: '/poster_circuithunt.png',
     title: 'Circuit Hunt',
+    subtitle: 'The main task of CIRCUIT HUNT is to solve the questions given in the QR codes and finally build a circuit using the answers they obtained, on a circuit building platform-multisim or by using hardware.',
     open:false
     // pageUrl: '/RoboticEvents'
   },
   {
     id: 'world-5',
     imgUrl: '/cover.png',
-    Poster: '/cover.png',
+    Poster: '/poster_valorant.png',
     title: 'LAN Gaming  -Valorant',
+    subtitle: 'A LAN Gaming is a valorant League organized for those who are enthusiastic about PC gaming. You have to bring your own laptops and a LAN connection is provided from our end for seamless experience while playing.',
     open:false
     // pageUrl: '/TechnicalEvents'   2. smart dustbin 1.tetris 2.target practice 3.theme based workshop 4. vr maze
   },
@@ -122,15 +132,17 @@ export const exploreWorlds51 = [
     id: 'world-6',
     imgUrl: '/planet13.jpg',
     Poster: '/planet13.jpg',
-    title: 'Tetris Game',
+    title: 'The Pong Game',
+    subtitle: 'The pong game is the two-dimensional game that simulates table tennis kind of competition.It consists of a ball bouncing on a screen and each of the players is provided with slider to hit the ball.The position of slider is adjusted using a knob which will be provided at the either sides of the screen.',
     open:false
     // pageUrl: '/Poster'
   },
   {
     id: 'world-7',
     imgUrl: '/planet-02.png',
-    Poster: '/planet-02.png',
+    Poster: '/poster_targetpractice.png',
     title: 'Target Practice',
+    subtitle: 'We always boast about our aim in FPS games with our friends. Target practice is a fun way to test our aim in real life. Target practice works by a random target going up with an LDR sensor right at the bull’s eye. Our gun here has a laser which when shot right at the LDR sensor the score count goes up and another random target pops up. In about a minute’s time, the person with maximum score wins the game.',
     open:false
     // pageUrl: '/InspireAndIgnite'
   },
@@ -138,23 +150,26 @@ export const exploreWorlds51 = [
     id: 'world-8',
     imgUrl: '/Planet24.jpg',
     Poster: '/Planet24.jpg',
-    title: 'Theme Based Workshop',
+    title: 'Codathon',
+    subtitle: 'Codathon is a coding competition exclusively for BE 1st and 2nd year students in which they have to solve 3 questions within a span of 60 minutes. Out of the 3 questions, one is easy, one is medium and the other is hard. Participants should bring a laptop along with them to participate in the contest.',
     open:false
     // pageUrl: '/ProjectExpo'
   },
   {
     id: 'world-9',
     imgUrl: '/Planet25.jpg',
-    Poster: '/Planet25.jpg',
+    Poster: '/poster_vrmaze.png',
     title: 'VR Maze',
+    subtitle: 'VR Maze gives you a immense virtual experience and you need to solve a maze in side by side  passing hurdles in allocated time. ',
     open:false
     // pageUrl: '/RoboticEvents'
   },
   {
     id: 'world-10',
     imgUrl: '/cover.png',
-    Poster: '/cover.png',
-    title: 'Smart Dustbin',
+    Poster: '/poster_whackamole.png',
+    title: 'Whack-A-Mole',
+    subtitle: 'Whack-a-mole" is a classic arcade game where players use a mallet to hit mechanical moles as they pop up from different holes. The game is usually played against a clock, with the goal being to hit as many moles as possible within the time limit. The traditional "Whack-a-mole" has been remodified to use lasers and sensors instead of a mallet. The game is timed, so players must work quickly to earn as many points as possible before time runs out. Their scores are displayed on a small LCD screen or a set of 7-segment displays.',
     open:false
     // pageUrl: '/TechnicalEvents'   2. smart dustbin 1.tetris 2.target practice 3.theme based workshop 4. vr maze
   },
