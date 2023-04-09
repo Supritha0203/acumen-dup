f8836280164a7b1df338fdaba948cf37539bfb9d
Supritha0203 <102866536+Supritha0203@users.noreply.github.com>
2023-04-09 21:57:26 +0530

    Update layout.js

--- pages/layout.js
+++ pages/layout.js
@@ -2,11 +2,11 @@ import '../styles/globals.css';
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <head>
+    <Head>
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
       
-    </head>
+    </Head>
     <body>{children}</body>
   </html>
 );
